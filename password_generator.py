c7ea93f2463faca15e8e8e0a1557a71aef67b70f
Thisura Manjitha Samarakoon <thissamarakoon@gmail.com>
2022-10-09 18:48:26 +0530

    password generator in python

---
+++ password_generator.py
@@ -0,0 +1,15 @@
+import random
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = "0123456789"
+symbol = "`-=[]\;,./~!@#$%^&*()_+{}|:<>?"
+
+def password_generator():
+    print("Your generated password is ")
+
+def readable_password_generator():
+    print("Your generated password is ")
+
+password_generator()
+readable_password_generator()
