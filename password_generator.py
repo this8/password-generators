1ebff28e01e1d4c9be735b6923962c7c3981aaf9
Thisura Manjitha Samarakoon <thissamarakoon@gmail.com>
2022-10-09 22:02:21 +0530

    completed code for readable password generator in python

--- password_generator.py
+++ password_generator.py
@@ -1,18 +1,36 @@
 import random
 
-lower_case = "abcdefghijklmnopqrstuvwxyz"
-upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-number = "0123456789"
-symbol = "`-=[]\;,./~!@#$%^&*()_+{}|:<>?"
+lower_case = "bcdfghjklmnpqrstvwxyz"
+upper_case = "BCDFGHJKLMNPQRSTVWXYZ"
+vowels = "aAeEiIoOuU"
+numbers = "0123456789"
+symbols = "`-=[]\;,./~!@#$%^&*()_+{}|:<>?"
 length_for_password = 12
 
 def password_generator():
-    use_for = lower_case + upper_case + number + symbol
-    password = "".join(random.sample(use_for, length_for_password))
+    characters = lower_case + upper_case + vowels + numbers+ symbols
+    password = "".join(random.sample(characters, length_for_password))
     print("Your generated password is " + password)
 
 def readable_password_generator():
-    print("Your generated password is ")
+    letter_characters = lower_case + upper_case
+    letter1 = "".join(random.sample(letter_characters, 1))
+    letter2 = "".join(random.sample(letter_characters, 1))
+    vowel = "".join(random.sample(vowels, 1))
+    random_number = "".join(random.sample(numbers, 1))
+    set1 = letter1 + vowel + letter2 + random_number
+    letter1 = "".join(random.sample(letter_characters, 1))
+    letter2 = "".join(random.sample(letter_characters, 1))
+    vowel = "".join(random.sample(vowels, 1))
+    random_symbol = "".join(random.sample(symbols, 1))
+    set2 = letter1 + vowel + letter2 + random_symbol
+    letter1 = "".join(random.sample(letter_characters, 1))
+    letter2 = "".join(random.sample(letter_characters, 1))
+    vowel = "".join(random.sample(vowels, 1))
+    random_number = "".join(random.sample(numbers, 1))
+    set3 = letter1 + vowel + letter2 + random_number
+    password = set1 + set2 + set3
+    print("Your readable generated password is " + password)
 
 password_generator()
 readable_password_generator()
