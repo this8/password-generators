c18d036c147d98f212575dca9dce5880267f4e7e
tathiracs <115966742+tathiracs@users.noreply.github.com>
2022-10-17 11:48:47 +0530

    random password generator function body

--- main.swift
+++ main.swift
@@ -16,8 +16,16 @@ let length_for_password = 12
 
 let characters = lower_case + upper_case + vowels + numbers + symbols
 
-func passwordGenerator(characters: String, length: Int) -> String {
-    return "hey"
+func passwordGenerator(inputCharacters: String, pwdLength: Int) -> String {
+    let inputCharactersLength = UInt32(inputCharacters.count)
+    var returnValue = ""
+    for _ in 0 ..< pwdLength {
+            let randomPosition = arc4random_uniform(inputCharactersLength)
+        //var character = inputCharacters.randomElement()
+            //returnValue += character
+        }
+        
+        return returnValue
 }
 
 func readablePasswordGenerator(characters: String, length: Int) -> String {
