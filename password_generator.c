c914a747436eff6ea266c4cb2bd1923d6c5e57a6
KushaniKaushalya <kushanikaushalya456@gmail.com>
2022-10-18 22:48:15 +0530

    Removed comments in password_generator.c

--- password_generator.c
+++ password_generator.c
@@ -1,42 +1,37 @@
 // password generator in C
 
-// C program for the above approach
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-// Function to randomly generates password
-// of length N
+
 void randomPasswordGeneration(int N)
 {
-	// Initialize counter
+
 	int i = 0;
 
 	int randomizer = 0;
 
-	// Seed the random-number generator
-	// with current time so that the
-	// numbers will be different every time
+
 	srand((unsigned int)(time(NULL)));
 
-	// Array of numbers
+	
 	char numbers[] = "0123456789";
 
-	// Array of small alphabets
+
 	char letter[] = "abcdefghijklmnoqprstuvwyzx";
 
-	// Array of capital alphabets
+	
 	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
 
-	// Array of all the special symbols
+	
 	char symbols[] = "!@#$^&*?";
 
-	// Stores the random password
+
 	char password[N];
 
-	// To select the randomizer
-	// inside the loop
+	
 	randomizer = rand() % 4;
 
 	// Iterate over the range [0, N]
@@ -65,14 +60,12 @@ void randomPasswordGeneration(int N)
 	}
 }
 
-// Driver Code
 int main()
 {
-	// Length of the password to
-	// be generated
+	
 	int N = 10;
 
-	// Function Call
+
 	randomPasswordGeneration(N);
 
 	return 0;
